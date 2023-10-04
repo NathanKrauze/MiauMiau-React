a67b9f1a6342e2c8161a74abe4aeecc2a9a38164
NathanKrauze <krauzenathan@gmail.com>
2023-10-04 16:31:10 -0300

    feat: signup page working

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
-import styled from "styled-components";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useRoutes } from "react-router-dom";
 import SignInPage from "./pages/LoginPage/LoginPage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
 import ManagePage from "./pages/ManagePage/ManagePage";
 import HomePage from "./pages/HomePage/HomePage";
 import AddCatPage from "./pages/AddCatPage/AddCatPage";
 import CatPage from "./pages/CatPage/CatPage";
+import AuthContext from "./contexts/AuthContext";
+import { useState } from "react";
 
 export default function App() {
+
+  const [token, setToken] = useState("")
 
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/sign-in" element={<SignInPage/>}/>
-          <Route path="/sign-up" element={<SignUpPage/>}/>
-          <Route path="/home" element={<HomePage/>}/>
-          <Route path="/" element={<CatPage/>}/>
-          <Route path="/Manage" element={<ManagePage/>}/>
-          <Route path="/Add" element={<AddCatPage/>}/>
-        </Routes>
+        <AuthContext.Provider value={{token, setToken}}>
+          <Routes>
+            <Route path="/" element={<SignInPage />} />
+            <Route path="/sign-up" element={<SignUpPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/cat" element={<CatPage />} />
+            <Route path="/manage" element={<ManagePage />} />
+            <Route path="/add" element={<AddCatPage />} />
+          </Routes>
+        </AuthContext.Provider>
       </BrowserRouter>
     </>
   )
--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,20 +1,74 @@
 import { styled } from "styled-components";
 import text_logo from "../../assets/text_logo.png";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function SignUpPage(){
+
+    const[name, setName] = useState("")
+    const[cpf, setCpf] = useState("")
+    const[phone, setPhone] = useState("")
+    const[email, setEmail] = useState("")
+    const[password, setPassword] = useState("")
+    const[confirmPassword, setConfirmPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    function signUp(e) {
+
+        e.preventDefault();
+
+        if (password !== confirmPassword) return alert("Password must be equal!");
+
+        axios.post(`${import.meta.env.VITE_API_URL}/signup`, { name, cpf, phone, email, password, confirmPassword })
+            .then(() => navigate("/home"))
+            .catch((err) => alert(err.message))
+
+    }
+
+
     return(
         <SignUpContainer>
             <img src={text_logo} alt="logo" />
-            <form>
-                <input placeholder="Nome" type="text"></input>
-                <input placeholder="CPF" type="text"></input>
-                <input placeholder="Telefone" type="text"></input>
-                <input placeholder="E-mail" type="email"></input>
-                <input placeholder="Senha" type="password"></input>
-                <input placeholder="Confirme sua senha" type="password"></input>
+            <form onSubmit={signUp}>
+                <input 
+                    placeholder="Nome" 
+                    type="text"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    required></input>
+                <input 
+                    placeholder="CPF" 
+                    type="text"
+                    value={cpf}
+                    onChange={e => setCpf(e.target.value)}
+                    required></input>
+                <input 
+                    placeholder="Telefone" 
+                    type="text"
+                    value={phone}
+                    onChange={e => setPhone(e.target.value)}></input>
+                <input 
+                    placeholder="E-mail" 
+                    type="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}></input>
+                <input 
+                    placeholder="Senha" 
+                    type="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}></input>
+                <input 
+                    placeholder="Confirme sua senha" 
+                    type="password"
+                    value={confirmPassword}
+                    onChange={e => setConfirmPassword(e.target.value)}></input>
                 <button type="submit">Cadastrar</button>
             </form>
-            <LinkSignIn>Já possui conta? faça o login!</LinkSignIn>
+            <Link to={'/'}>
+                <LinkSignIn>Já possui conta? faça o login!</LinkSignIn>
+            </Link>
         </SignUpContainer>
     )
 };
@@ -35,9 +89,12 @@ const SignUpContainer = styled.div`
         justify-content: space-between;
         margin-top: 24px;
     }
-
+    a{
+        color: black;
+        text-decoration: none;
+    }
 `
 
-const LinkSignIn = styled.div`
+const LinkSignIn = styled.p`
     margin-top: 8px;
 `
